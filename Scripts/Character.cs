d8fbeafa966ee547447a3542cf480f4543c7dc39
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-12 20:07:48 +0000

    Linecast for jumping and Front Detection

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+	//for moving
+	float move;
 	public float maxSpeed = 10f;
 	public bool facingRight = true;
-
 	Animator anim;
 
-	bool grounded = false;
+	//for jumping
+	public bool grounded = false;
 	public Transform groundCheck;
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
-	public float jumpForce = 30;
-	float move;
+	public float jumpForce = 30f;
+
+	//for Collision Raycasting
+	//behind
+	public Transform lTBegin,lTEnd;
+	public Transform lBBegin,lBEnd;
+	//front
+	public Transform rTBegin,rTEnd;
+	public Transform rBBegin, rBEnd;
+	//down
+	public Transform bRBegin, bREnd;
+	public Transform bLBegin, bLEnd;
+	public bool isColliding;
 
 	// Use this for initialization
 	void Start () 
@@ -24,25 +37,53 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		DebugLines();
+		//grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		SetGrounded ();
 		anim.SetBool ("ground", grounded);
 		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 
 		Jump ();
 		move = Input.GetAxis ("Horizontal");
-		CheckHorizontalColission ();
-		anim.SetFloat ("speed", Mathf.Abs (move));
-		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
-
 		if (move < 0 && facingRight)
 			Flip ();
 		else 
 			if (move > 0 && !facingRight)
 				Flip ();
+		CheckFrontColission ();
+		anim.SetFloat ("speed", Mathf.Abs (move));
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 
 	}
 
+	void SetGrounded()
+	{
+		if(Physics2D.Linecast (bRBegin.position, bREnd.position, whatIsGround))
+		{
+			grounded = true;
+		}else
+		{
+			if(Physics2D.Linecast (bLBegin.position, bLEnd.position, whatIsGround))
+			{
+				grounded = true;
+			}else
+			{
+				grounded = false;
+			}
+		}
+
+	}
+
+	void DebugLines()
+	{
+		Debug.DrawLine(lTBegin.position, lTEnd.position, Color.green);
+		Debug.DrawLine(lBBegin.position, lBEnd.position, Color.magenta);
+		Debug.DrawLine(rTBegin.position, rTEnd.position, Color.red);
+		Debug.DrawLine(rBBegin.position, rBEnd.position, Color.red);
+		Debug.DrawLine(bRBegin.position, bREnd.position, Color.red);
+		Debug.DrawLine(bLBegin.position, bLEnd.position, Color.red);
+
+	}
 	//flip image
 	void Flip ()
 	{
@@ -63,14 +104,22 @@ public class Character : MonoBehaviour {
 
 	}
 
-	//beginning for checking collision with raycast
-	void CheckHorizontalColission () {
-		bool isColliding = false;
-		RaycastHit hit;
-		if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.left), out hit) || Physics.Raycast (transform.position, transform.TransformDirection(Vector3.right), out hit))
+	//check front colission for being stuck in air
+	void CheckFrontColission () {
+
+		if(Physics2D.Linecast (rTBegin.position, rTEnd.position, 1 << LayerMask.NameToLayer("Ground")))
 		{
 			isColliding = true;
-		}
+		}else
+			{
+				if(Physics2D.Linecast (rBBegin.position, rBEnd.position, 1 << LayerMask.NameToLayer("Ground")))
+				{
+					isColliding = true;
+				}else
+					{
+						isColliding = false;
+					}
+			}
 			/*if(hit.transform.name.Contains("MovingPlatform"))
 			{
 				Transform movingPlatform  = hit.collider.transform;
@@ -78,7 +127,7 @@ public class Character : MonoBehaviour {
 				
 			}*/
 			
-		if (isColliding) 
+		if (isColliding && !grounded) 
 		{
 			move = 0;
 		}
